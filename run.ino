c52ea23b09bb6c41634b7a1937e65e9cd48ccd7e
Partha Sardar <partha746@gmail.com>
2019-12-25 17:52:30 +0530

    Enabled OTA

--- run.ino
+++ run.ino
@@ -1,25 +1,53 @@
-#include <BlockDriver.h>
-#include <SdFatConfig.h>
-#include <SdFat.h>
-#include <MinimumSerial.h>
-#include <SysCall.h>
-#include <sdios.h>
-#include <FreeStack.h>
-
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ArduinoOTA.h>
 #include "fauxmoESP.h"
-#include "credentials.h"
+#include <BlynkSimpleEsp8266.h>
+#include "wifi_passphrares.h"
 
 fauxmoESP fauxmo;
 
 #define Relay D0
 #define ID_Relay "Computer"
 
+void OTA(){
+  ArduinoOTA.setHostname("Computer_trigger");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+}
+
 void wifiSetup() {
     WiFi.mode(WIFI_STA);
-    Serial.printf("[WIFI] Connecting to %s ", WIFI_SSID);
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
+    Serial.printf("[WIFI] Connecting to %s ", SSID1);
+    WiFi.begin(SSID1, WifiPass);
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
         delay(500);
@@ -42,34 +70,33 @@ void tog(){
 }
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println();
+  Serial.begin(9600);
+  OTA();
+  
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, HIGH);
 
-    pinMode(Relay, OUTPUT);
-    digitalWrite(Relay, HIGH);
+  wifiSetup();
 
-    wifiSetup();
+  fauxmo.createServer(true);
+  fauxmo.setPort(80);
+  fauxmo.enable(true);
+  fauxmo.addDevice(ID_Relay);
 
-    fauxmo.createServer(true);
-    fauxmo.setPort(80);
-
-    fauxmo.enable(true);
-
-    fauxmo.addDevice(ID_Relay);
-
-    fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
-        Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-        if (strcmp(device_name, ID_Relay)==0) {
-            if(state){
-              tog();
-            }
-            else{
-              tog();
-            }          
-        }
-    });
+  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
+//      Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+      if (strcmp(device_name, ID_Relay)== 0) {
+          if(state){
+            tog();
+          }
+          else{
+            tog();
+          }          
+      }
+  });
 }
 
 void loop() {
-    fauxmo.handle();
+  ArduinoOTA.handle();    
+  fauxmo.handle();
 }
